e0c7fade96a23c0a147177227204b7f8fe5bf6fd
Jacob Ebey <jacob.ebey@live.com>
2015-05-16 21:40:01 -0700

    Added test case and moved to a child directory.

---
+++ MTSharp.Tests/MTSharpTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+
+namespace MTSharp.Tests
+{
+    [TestClass]
+    public class MTSharpTests
+    {
+        [TestMethod]
+        public void VerifyMTConnectCanRetrieveDeviceStreamsAndData()
+        {
+            MTConnect connect = new MTConnect("http://agent.mtconnect.org");
+            IEnumerable<Result> results = connect.Current()["VMC-3Axis"];
+
+            if (results == null) Assert.Fail();
+
+            Sample result = results.FirstOrDefault(r =>
+            {
+                Sample sample = r as Sample;
+
+                if (sample == null) return false;
+
+                return sample?.DataItem?.Name == "Sspeed";
+            }) as Sample;
+
+            if (result == null) Assert.Fail();
+        }
+    }
+}
